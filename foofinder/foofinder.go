6ab8120bdeb50834ba16f6bfd32016a56426fc10
Dimas Latief <latief.selatan@gmail.com>
2017-08-06 19:17:39 +0700

    Messing it again

--- foofinder/foofinder.go
+++ foofinder/foofinder.go
@@ -4,10 +4,10 @@ import "errors"
 
 func IsItFoo(word string) (bool, error) {
 	switch word {
-	case "foo":
+	case "fooZ":
 		return true, nil
 	case "bar":
-		err := errors.New("Oh noooooo, it's bar!")
+		err := errors.New("Oh noooooo, it's barZ!")
 		return false, err
 	}
 	return false, nil
